521bbb5d792ae6f88dd4a4ac1735d02f1139ba58
Viktor von Drakk <viktorvondrakk@gmail.com>
2017-05-21 12:15:19 -0700

    updated about copy to include overview info of the udemy tutorial

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function () {
     return (
-      <h3>Examples</h3>
+      <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out</p>
+        <ol>
+          <li>
+            <Link to="/?location=Seattle">Seattle, WA</Link>
+          </li>
+          <li>
+            <Link to="/?location=Rio">Rio, Brazil</Link>
+          </li>
+        </ol>
+      </div>
     );
   }
 });
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,7 +8,7 @@ var Main = React.createClass({
       <div>
         <Nav/>
         <div className="row">
-          <div className="columns medium-6 large-4 small-centered">
+          <div className="column small-centered">
             {this.props.children}
           </div>
         </div>
