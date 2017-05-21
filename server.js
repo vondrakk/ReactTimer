981500f1c09bc50774041d021b4387c39e29c957
Viktor von Drakk <viktorvondrakk@gmail.com>
2017-05-21 09:31:50 -0700

    fixed server.js redirect problem

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const PORT = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   if (req.headers["x-forwarded-proto"] === 'http') {
     next();
+  } else {}
+    res.redirect('http://' + req.hostname + req.url);
   }
-  res.redirect('http://' + req.hostname + req.url);
 });
 app.use(express.static('public'));
 app.listen(PORT, function () {
