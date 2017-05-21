aa4e59c4f0cf60c092f5d89fa7d52b813a45f928
Viktor von Drakk <viktorvondrakk@gmail.com>
2017-05-21 09:59:49 -0700

    updated about page html

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var About = React.createClass({
   render: function () {
     return (
-      <h3>About Component</h3>
+      <div>
+        <h3>About</h3>
+        <p>Welcome to the About page</p>
+      </div>
     );
   }
 });
--- public/bundle.js
+++ public/bundle.js
@@ -29721,9 +29721,18 @@
 	
 	  render: function render() {
 	    return React.createElement(
-	      'h3',
+	      'div',
 	      null,
-	      'About Component'
+	      React.createElement(
+	        'h3',
+	        null,
+	        'About'
+	      ),
+	      React.createElement(
+	        'p',
+	        null,
+	        'Welcome to the About page'
+	      )
 	    );
 	  }
 	});
