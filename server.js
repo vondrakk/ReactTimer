e53f5c47ed6e58e63ca53597864efb32ad0bd025
Viktor von Drakk <viktorvondrakk@gmail.com>
2017-05-21 09:33:50 -0700

    fixed server.js typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   if (req.headers["x-forwarded-proto"] === 'http') {
     next();
-  } else {}
+  } else {
     res.redirect('http://' + req.hostname + req.url);
   }
 });
